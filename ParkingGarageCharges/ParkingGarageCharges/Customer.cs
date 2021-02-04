c48af51d22f9840ab4ec4210c7deb2e237e0c1ee
jazdenmccall <mccall.jazden0@gmail.com>
2021-02-04 17:19:33 -0600

    Add files via upload
    first Commit

---
+++ ParkingGarageCharges/ParkingGarageCharges/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingGarageCharges
+{
+    class Customer
+    {
+        const double MinimumCharge = 2.0;
+        const double AdditionalCharge = 0.5;
+        const double MaximumCharge = 10.0;
+        const double MaximumHours = 24;
+        const int BaseHours = 3;
+        private double _hours;
+
+        public Customer(double hours)
+        {
+            Hours = hours;
+        }
+        public double Hours
+        {
+            get { return _hours; }
+            set
+            {
+                if (value < 0 || value > 24)
+                    throw new Exception("Invalid entry. Please enter a number greater than 0 and less than 25");
+                else
+                    _hours = value;
+            }
+        }
+
+        public double CalculateCharges()
+        {
+            double totalCharge;
+            if (Hours <= BaseHours)
+                totalCharge = Hours * MinimumCharge;
+            else
+                totalCharge = MinimumCharge + ((Hours - BaseHours) * AdditionalCharge);
+            if (totalCharge > MaximumCharge)
+                totalCharge = MaximumCharge;
+            return totalCharge;
+        }
+
+    }
+}
---
+++ ParkingGarageCharges/ParkingGarageCharges/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ParkingGarageCharges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCustomers;
+            double hours;
+            double total = 0.0;
+            Console.WriteLine("Please enter how many customers parked in the garage yesterday");
+            numberOfCustomers = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= numberOfCustomers; i++)
+            {
+                Console.WriteLine("Please enter how many hours Customer {0} parked", i);
+                hours = Convert.ToDouble(Console.ReadLine());
+                Customer cust = new Customer(hours);
+                Console.WriteLine("Customer {0} parking charges are: {1:C2}", i, cust.CalculateCharges());
+                total += cust.CalculateCharges();
+            }
+            Console.WriteLine("Total parking charges for all customers from yesterday: {0:C2}", total);
+            Console.ReadLine();
+        }
+    }
+
+}
